e98044a9f3953162f9fa763f0b677b542f9b1d77
hmtanbir <tanbir2025@gmail.com>
2017-09-05 14:37:53 +0600

    updates application controller to prevent CSRF exception from being raised

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -17,9 +17,6 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eq(user.email)
       expect(user_response[:name]).to eq(user.name)
-    end
-
-    
+    end    
   end
-
 end
