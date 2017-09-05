c8fc5b590a71e9558f1fef80000b04d98f1fda7c
hmtanbir <tanbir2025@gmail.com>
2017-09-05 22:55:20 +0600

    REST API for User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,78 @@
 class Api::V1::UsersController < ApplicationController
-	respond_to :json
+  before_action :set_user, only: [:update, :destroy]
+  respond_to :json
+
+  def index
+    begin
+      respond_with User.all
+    rescue => e
+      Rails.logger.error { "Error generated for nil user: #{user_id}, #{e.message} #{e.backtrace.join("\n")}" }
+      nil
+    end
+  end
 
   def show
-    respond_with User.find(params[:id]) 
+    begin
+      respond_with User.find(user_id)
+    rescue => e
+      Rails.logger.error { "Error generated for nil user: #{user_id}, #{e.message} #{e.backtrace.join("\n")}" }
+      nil
+    end
+  end
+
+  def create
+    begin
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: 201, location: api_user_url(@user)
+      else
+        render json: { errors: @user.errors }, status: 422
+      end
+    rescue => e
+      Rails.logger.error { "Error generated while creating user on: #{e.message} #{e.backtrace.join("\n")}" }
+      nil
+    end
+  end
+
+  def update
+    begin
+      if @user.update(user_params)
+        render json: @user, status: 200, location: api_user_url(@user)
+      else
+        render json: { errors: @user.errors }, status: 422
+      end
+    rescue => e
+      Rails.logger.error { "Error generated while finding user for: #{user_id}, #{e.message} #{e.backtrace.join("\n")}" }
+      nil
+    end
+  end
+
+  def destroy
+    begin
+      @user.destroy
+      head 204
+    rescue => e
+      Rails.logger.error { "Error generated while destroying user for: #{user_id}, #{e.message} #{e.backtrace.join("\n")}" }
+      nil
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    begin
+      @user = User.find(user_id)
+    rescue => e
+      Rails.logger.error { "Error generated while finding user for: #{user_id}, #{e.message} #{e.backtrace.join("\n")}" }
+      nil
+    end
+  end
+
+  def user_id
+    params[:id]
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,22 +1,134 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  let(:user) { FactoryGirl.create :user }	 
-  before(:each) { request.headers['Accept'] = "application/vnd.helpline.v1" }
+  let(:valid_user) { FactoryGirl.attributes_for :user }
+  let(:invalid_user) { FactoryGirl.attributes_for :user, name: nil }
+  let(:user) { User.create! valid_user }
+  let(:user_response) { JSON.parse(response.body, symbolize_names: true) }
+  before(:each) { request.headers['Accept'] = 'application/vnd.helpline.v1' }
 
-  describe "GET #show" do
-    before(:each) do      
-      get :show, params: { id: user.id, format: :json }
+  describe 'GET #index' do
+    it 'returns 200 success code' do
+      get :index, format: :json
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET #show' do
+    context 'valid user' do
+      before(:each) do
+        get :show, params: { id: user.id }, format: :json
+      end
+
+      it 'returns 200 success code' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns the information about a reporter on a hash' do
+        expect(user_response[:email]).to eq(user.email)
+        expect(user_response[:name]).to eq(user.name)
+      end
     end
 
-    it "checks if responds successfully" do        
-      expect(response).to be_success
+    context 'invalid user' do
+      before(:each) do
+        get :show, params: { id: 100 }, format: :json
+      end
+
+      it 'returns 204 code' do
+        expect(response.status).to eq(204)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'valid user' do
+      it 'creates a new User' do
+        expect {
+          post :create, params: { user: valid_user }, format: :json
+        }.to change(User, :count).by(1)
+      end
+
+      it 'returns 201 success code' do
+        post :create, params: { user: valid_user }, format: :json
+        expect(response.status).to eq(201)
+      end
+
+      it 'renders the json representation for the user record just created' do
+        post :create, params: { user: valid_user }, format: :json
+        expect(user_response[:email]).to eql valid_user[:email]
+      end
     end
 
-    it "returns the information about a reporter on a hash" do
-      user_response = JSON.parse(response.body, symbolize_names: true)
-      expect(user_response[:email]).to eq(user.email)
-      expect(user_response[:name]).to eq(user.name)
-    end    
+    context 'invalid user' do
+      before(:each) do
+        post :create, params: { user: invalid_user }, format: :json
+      end
+
+      it 'returns 422 error code' do
+        expect(response.status).to eq(422)
+      end
+
+      it 'renders an errors json' do
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on when the user could not be created' do
+        expect(user_response[:errors][:name]).to include "can't be blank"
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    let(:new_attributes) { { email: "newmail@example.com" } }
+    before(:each) do
+      @user = User.create! valid_user
+    end
+
+    context 'when user is successfully updated' do
+      before(:each) do
+        patch :update, params: { id: @user.id, user: new_attributes }, format: :json
+      end
+
+      it 'returns 200 success code' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'renders the json representation for the updated user' do
+        expect(user_response[:email]).to eql 'newmail@example.com'
+      end
+    end
+
+    context 'when user is not created' do
+      before(:each) do
+        patch :update, params: { id: @user.id, user: { email: 'bademail.com' } }, format: :json
+      end
+
+      it 'returns 422 code' do
+        expect(response.status).to eq(422)
+      end
+
+      it 'renders an errors json' do
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on whye the user could not be created' do
+        expect(user_response[:errors][:email]).to include 'is invalid'
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroys the requested user' do
+      user
+      expect {
+        delete :destroy, params: { id: user.id }, format: :json
+      }.to change(User, :count).by(-1)
+    end
+
+    it 'returns 204 code' do
+      delete :destroy, params: { id: user.id }, format: :json
+      expect(response.status).to eq(204)
+    end
   end
 end
